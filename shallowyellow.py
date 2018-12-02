c6ffc7a78fcbbf70b7d7b19b4778d5a0bb4c6662
Dan Saubert <dsaubert@nmu.edu>
2018-12-01 20:55:16 -0500

    It only took 9 hours but tensorflow is running happily on my machine, all imports successful.

---
+++ shallowyellow.py
@@ -0,0 +1,10 @@
+#Daniel Saubert - CS495 Senior Project
+#Shallow Yellow is a reinforcement based machine learning program that uses
+#OpenAI Gym to interface with environments
+
+import tensorflow as tf
+import gym
+import numpy as np
+
+for n in range(10):
+    print("I will not become self aware.")
