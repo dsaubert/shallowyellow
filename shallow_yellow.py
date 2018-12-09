3e3d37efeefaad54706bb6bcba3b383434075591
Dan Saubert <dsaubert@nmu.edu>
2018-12-08 22:49:07 -0500

    Learns "a little", regularly reaching scores in the 500-1000 range

--- shallow_yellow.py
+++ shallow_yellow.py
@@ -13,19 +13,20 @@ import numpy as np
 from scipy import misc
 import random
 from skimage import color
+import gc
 
-total_episodes = 100000
-memory_replay_sample_size = 32
-minimum_replay_size = 50000
-save_model_frequency = 100
+total_episodes = 10000
+memory_replay_sample_size = 16
+minimum_replay_size = 5000
+save_model_frequency = 100 #Episodes in between saving iteration
 
 class shallow_yellow:
     def __init__(self, input_image_size, available_actions):
         self.state_size = input_image_size
         self.action_size = available_actions
-        self.memories = deque(maxlen = 1000000) #How long should the AI hold onto memories? Once the deque is full, it begins forgetting old memories from when the AI sucked
+        self.memories = deque(maxlen = 50000) #How long should the AI hold onto memories? Once the deque is full, it begins forgetting old memories from when the AI sucked
         self.epsilon = 0.95 #Initial Exploration rate, as a percent probability that the chosen action will be random
-        self.epsilon_decay = 0.9 #How quickly the AI stops exploring and starts to trust its decsions
+        self.epsilon_decay = 0.99 #How quickly the AI stops exploring and starts to trust its decsions
         self.epsilon_minimum = 0.025 #The lowest possible exploration rate
         self.learning_rate = 0.001 #The size of each step of gradient descent
         self.discount_rate = 0.95 #Ratio of future action reward to current action reward. Ensures success in the long run while prioritizing realtime success
@@ -38,7 +39,7 @@ class shallow_yellow:
         model.add(Flatten())
         model.add(Dense(256, activation="relu"))
         model.add(Dense(self.action_size, activation="linear"))
-        model.compile(optimizer="adam", loss="mse")
+        model.compile(optimizer="adam", loss="mse") #default adam LR is 0.001, mse is defined in Q-eqn of (optimal-guessed)^2
         return model
 
     def print_network_structure_graph(self):
@@ -86,6 +87,8 @@ if __name__ == "__main__": #This is structured around the Q-Learning Algorithim
     action_size = env.action_space.n #Asks the environment how many possible action can be taken
     SY = shallow_yellow(state_size, action_size)
     game_over = False
+    reward=0
+    gc.enable()
 
     for episode in range(total_episodes):
         current_state = deque(maxlen=4)#To give the neural network a concept of trajectory and time, we train it on the last 4 frames of gameplay
@@ -96,7 +99,8 @@ if __name__ == "__main__": #This is structured around the Q-Learning Algorithim
         for t in range(5000):
             #env.render() #Remove for quicker training, allows you to watch the game be played
             action = SY.choose_action(current_state)
-            next_frame, reward, game_over, debug_info = env.step(action)
+            next_frame, current_reward, game_over, debug_info = env.step(action)
+            reward+= current_reward
             next_state = current_state.copy()
             next_state.append(SY.rgb2gray(next_frame)) #don't want to change current state, so a copy is used.
             SY.remember(current_state, action, reward, game_over, next_state)
@@ -105,7 +109,7 @@ if __name__ == "__main__": #This is structured around the Q-Learning Algorithim
                 break
             current_state = next_state
             if (len(SY.memories) > minimum_replay_size): #if there are enough memories available, perform experience replay
-                print("Performing Memory Replay")
+                #print("Performing Memory Replay")
                 SY.perform_memory_replay(memory_replay_sample_size)
             #print("Timestep: {}".format(t), end=" ")
         if episode % save_model_frequency == 0:
